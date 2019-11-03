7b77e3345d6f864d9c9e28631c2b8634f974edd6
Daniel Schaefer <git@danielschaefer.me>
2019-11-03 16:49:59 +0100

    Wrap app start in try/catch for debugging
    Wrap in try/catch and print everything, to see the proper stacktrace
    even in non-debugging environments.
    This is especially useful if it crashes right after launching.

--- src/me/danielschaefer/sensorwrangler/javafx/App.kt
+++ src/me/danielschaefer/sensorwrangler/javafx/App.kt
@@ -17,6 +17,22 @@ class App: Application() {
     override fun start(primaryStage: Stage) {
         instance = this
 
+        // Wrap in try/catch and print everything, to see the proper stacktrace
+        // even in non-debugging environments.
+        // This is especially useful if it crashes right after launching.
+        try {
+            runMainWindow(primaryStage)
+        } catch (e: Throwable) {
+            println("Exception: $e occured")
+            println("With message ${e.message}")
+            println("And cause: ${e.cause}")
+            println("Stacktrace:")
+            for (x in e.stackTrace)
+                println(x)
+        }
+    }
+
+    private fun runMainWindow(primaryStage: Stage) {
         val dummyPowerSensor = RandomSensor(1000).apply {
             minValue = 0
             maxValue = 10
