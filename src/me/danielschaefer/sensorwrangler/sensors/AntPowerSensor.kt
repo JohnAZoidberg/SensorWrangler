075588a20129977e1c16a96268971cb51d893f31
Daniel Schaefer <git@danielschaefer.me>
2020-01-05 16:49:15 +0100

    Power: Add torque barycenter angle measurement

--- src/me/danielschaefer/sensorwrangler/sensors/AntPowerSensor.kt
+++ src/me/danielschaefer/sensorwrangler/sensors/AntPowerSensor.kt
@@ -5,10 +5,13 @@ import be.glever.ant.constants.AntPlusDeviceType
 import be.glever.ant.message.AntMessage
 import be.glever.ant.message.data.BroadcastDataMessage
 import be.glever.ant.usb.AntUsbDevice
+import be.glever.antplus.common.datapage.AbstractAntPlusDataPage
 import be.glever.antplus.common.datapage.registry.AbstractDataPageRegistry
 import be.glever.antplus.power.PowerChannel
 import be.glever.antplus.power.datapage.PowerDataPageRegistry
+import be.glever.antplus.power.datapage.background.*
 import be.glever.antplus.power.datapage.main.PowerDataPage10PowerOnly
+import javafx.application.Platform
 import me.danielschaefer.sensorwrangler.Measurement
 import kotlin.random.Random
 
@@ -21,10 +24,17 @@ class AntPowerSensor: AntPlusSensor<PowerChannel>() {
     private val cadenceMeasurement = Measurement(this, 0, Measurement.Unit.RPM).apply{
         description = "Cadence " + Random.nextInt(0, 100)
     }
-    private val powerDistributionMeasurment = Measurement(this, 0, Measurement.Unit.PERCENTAGE).apply{
+    private val powerDistributionMeasurement = Measurement(this, 0, Measurement.Unit.PERCENTAGE).apply{
         description = "Power distribution" + Random.nextInt(0, 100)
     }
-    override val measurements: List<Measurement> = listOf(powerMeasurement, cadenceMeasurement, powerDistributionMeasurment)
+    private val torqueBarycenterAngleMeasurement = Measurement(this, 0, Measurement.Unit.DEGREE).apply{
+        description = "Torque Barycenter Angle" + Random.nextInt(0, 100)
+    }
+    override val measurements: List<Measurement> = listOf(
+        powerMeasurement,
+        cadenceMeasurement,
+        powerDistributionMeasurement,
+        torqueBarycenterAngleMeasurement)
 
     override val registry: AbstractDataPageRegistry = PowerDataPageRegistry()
 
@@ -38,12 +48,56 @@ class AntPowerSensor: AntPlusSensor<PowerChannel>() {
         removeToggleBit(payLoad)
 
         val dataPage = registry.constructDataPage(payLoad)
-        if (dataPage is PowerDataPage10PowerOnly) {
-            powerMeasurement.addDataPoint(dataPage.instantaneousPower)
-            cadenceMeasurement.addDataPoint(dataPage.instantaneousCadence.toDouble())
+        Platform.runLater { addMeasurement(dataPage) }
+    }
 
-            if (dataPage.pedalPowerDistribution.percentage != 127)
-                powerDistributionMeasurment.addDataPoint(dataPage.pedalPowerDistribution.percentage.toDouble())
+    private fun addMeasurement(dataPage: AbstractAntPlusDataPage) {
+        // TODO: Check which measurements are supported
+        when (dataPage) {
+            is PowerDataPage3MeasurementOutput -> {
+                // TODO: Handle this data page
+                dataPage.numberOfDataTypes
+                dataPage.dataType
+                dataPage.scaledMeasurement
+                dataPage.timeStamp
+                dataPage.measurement
+                dataPage.scaleFactor
+            }
+            is PowerDataPage14TorqueBarycenter -> {
+                torqueBarycenterAngleMeasurement.addDataPoint(dataPage.torqueBarycenterAngle)
+            }
+            is PowerDataPageE0RightForceAngle -> {
+                // TODO: Handle this data page
+                dataPage.torque
+                dataPage.startAngle
+                dataPage.endAngle
+                dataPage.startPeakAngle
+                dataPage.endPeakAngle
+            }
+            is PowerDataPageE1LeftForceAngle -> {
+                // TODO: Handle this data page
+                dataPage.torque
+                dataPage.startAngle
+                dataPage.endAngle
+                dataPage.startPeakAngle
+                dataPage.endPeakAngle
+            }
+            is PowerDataPageE2PedalPosition -> {
+                // TODO: Handle this data page
+                dataPage.cadence
+                dataPage.leftPco
+                dataPage.rightPco
+                dataPage.riderPosition
+            }
+            is PowerDataPage10PowerOnly -> {
+                powerMeasurement.addDataPoint(dataPage.instantaneousPower.toDouble())
+                cadenceMeasurement.addDataPoint(dataPage.instantaneousCadence.toDouble())
+
+                dataPage.pedalPowerDistribution?.let {
+                    if (it.percentage != 127)
+                        powerDistributionMeasurement.addDataPoint(it.percentage.toDouble())
+                }
+            }
         }
     }
 
